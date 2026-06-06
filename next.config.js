21c9bcc477510206ffa5440df26998a390b210e4
Valba312 <145738603+Valba312@users.noreply.github.com>
2026-06-07 01:55:09 +0500

    пзд1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  telemetry: false, // Disable telemetry for faster builds
   async headers() {
     if (process.env.NODE_ENV !== "production") {
       return [{ source: "/:path*", headers: [{ key: "Cache-Control", value: "no-store" }] }];
